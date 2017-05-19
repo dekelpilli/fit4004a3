9de1adf042d44a6930a04948387055ec39fc37b8
Lawrence MacDonald <lawrence@isimplify.com.au>
2017-05-19 17:21:01 +1000

    Moved tests and created some broken, basic tweepy code

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+import tweepy
+
 if __name__ == "__main__":
-    print("Testing")
+    # We need consumer_key and consumer_secret from a valid Twitter application
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    api = tweepy.API(auth)
+
+    public_tweets = api.home_timeline()
+    for tweet in public_tweets:
+        print(tweet.text)
